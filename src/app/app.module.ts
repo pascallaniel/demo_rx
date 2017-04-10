382f30871d9f7bcc862732ee720722f35659b995
Pascal Laniel <xlanipa@ville.montreal.qc.ca>
2017-04-10 15:05:38 -0400

    Import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MergeComponent } from './merge/merge.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CombineLatestComponent } from './combine-latest/combine-latest.component';
+import { EventBusService } from './event-bus.service';
+import { ErrorComponent } from './error/error.component';
+import { CustomComponent } from './custom/custom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutocompleteComponent,
+    MergeComponent,
+    WeatherComponent,
+    CombineLatestComponent,
+    ErrorComponent,
+    CustomComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [EventBusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
